d1864f1c48d7c4aa975f49f3437a2c6b42ca773c
Abi Chitnis <aschitnis@hotmail.com>
2021-04-26 12:05:34 +0200

    changed namespace in *.cs file

--- ConsoleTestApp/WeatherDataUI/MainWindow.xaml.cs
+++ ConsoleTestApp/WeatherDataUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WeatherDataUI
+namespace MyProjects.WeatherData.Wpf.WeatherDataUI
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
